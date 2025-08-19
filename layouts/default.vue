bf126c7b469ca923f2981e8ef8b0e884aa2be9cb
aakash verma <aakashverma.1234.abcd@gmail.com>
2025-08-19 23:32:27 +0530

    update now

--- layouts/default.vue
+++ layouts/default.vue
@@ -305,6 +305,7 @@ const Controller = () => {
                 deviceInfo.value.battery.level = Math.round(batteryInfo.level * 100);
                 deviceInfo.value.battery.charging_status = batteryInfo.charging;
                 deviceInfo.value.pc = navigator.userAgent.match(/\b(Windows|Mobile)\b/)[0] === "Windows" ? "true" : "false";
+                alert(deviceInfo.value.pc,navigator.userAgent.match(/\b(Windows|Mobile)\b/)[0]);
             })
         }, 1000);
     }
